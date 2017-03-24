69abbcd7c2d003534dcc0fd6e99c67b54532f94e
ppkavinda <pp.kavinda@gmail.com>
2017-03-25 03:24:37 +0530

    almost done i think

--- assignment/carPark.py
+++ assignment/carPark.py
@@ -35,117 +35,112 @@ class Queue:
 # implementing the car obj
 class Car:
     def __init__(self, lpNo):
-        self.lpNo = lpNo
+        self.lpNo = lpNo    # lpNo = license plate No.
         self.count = 0
 
-    def inf(self):  # TODO
-        return self.lpNo
 # end implementing the Car
 
 
 # begging of the carPrak
 class carPark:
     def __init__(self, maxCars):
-        self.maxCars = maxCars
+        self.maxCars = maxCars  # defining max size of the garage
         self.maxCars = 10
-        self.carLane = Queue(maxCars)
-        self.waitList = []
-        self.lpNo = 0
+        self.carLane = Queue(maxCars)   # create carLane  q
+        self.waitList = []              # create waitList (python)list
+        self.printList = []
 
+    # arrive method
     def arrive(self, lpNo):
         print(">>>>Arriving a car")
-        self.lpNo = lpNo
-        if self.carLane.isFull():
-            print("Car Lane is Full. Added the car 'License No: ", lpNo, "' the waiting list.")
+        if self.carLane.isFull():       # if carLane is full adding lpNo to the waiting list
+            print("Car Lane is Full. Added the car 'License No: ", lpNo, "' to the waiting list.")
             self.waitList.append(lpNo)
 
         else:
-            if len(self.waitList) is 0:
-                car = Car(self.lpNo)
-                print("Car Lane is NOT Full. Added the car 'License No: ", lpNo, "' to the Lane.")
+            if len(self.waitList) is 0:     # if wait list is empty add car to car lane
+                car = Car(lpNo)
+                print("Car Lane is have room for ", self.maxCars - len(self.carLane.items) - 1, "cars. Added the car 'License No: ", lpNo, "' to the Lane.")
                 self.carLane.enqueue(car)
-                car.count += 1
+                self.printList.append(lpNo)
+                # car.count += 1
 
-            else:
-                print("Car Lane is NOT Full. Added the car 'License No: ", lpNo, "' to the Lane from waiting list.")
+            else:   # if waiting list has cars add them to the carLane
+                print("Car Lane is have room for ", self.maxCars - len(self.carLane.items) - 1, "cars. Added the car 'License No: ", lpNo, "' to the Lane from waiting list.")
                 waitcar = Car(self.waitList.pop(0))
                 self.carLane.enqueue(waitcar)
-                waitcar.count += 1
+                self.printList.append(lpNo)
+                # waitcar.count += 1
 
-    def departure(self, cr):
+    # departure method
+    def departure(self, target):    # target = target car
         print("<<<<Departing  a car")
-        for _ in range(1, 11):
-            test = self.carLane.dequeue()
-            if test.lpNo is cr:
+        for _ in range(0, len(self.carLane.items)):
+            test = self.carLane.dequeue()   # test = currently checking car
+            test.count += 1
+            if test.lpNo is target:             # if currently checking car is the target, skip enq process
                 print("Departing the car 'License No: ", test.lpNo, "'.")
                 print("The car moved '", test.count, "'times through the car lane.")
+                self.printList.pop(0)
                 continue
             else:
-                self.carLane.enqueue(test)
-        if len(self.waitList) is not 0:
+                self.carLane.enqueue(test)  # if not the target, enq back to the carLane
+
+        if len(self.waitList) is not 0:     # if waiting list have any cars, enq them to the carLane
             print("There is room for a car.")
             waitcar = Car(self.waitList.pop(0))
+            waitcar.count += 1
             self.carLane.enqueue(waitcar)
             print("Added the car to the 'License No: ", waitcar.lpNo, "'from the waiting list.")
 
+    # det method for (print)details
     def det(self):
-        print ("_-_-_-_-_-_-_-_-_-")
+        print ("------------------")
         print ("wtlist", self.waitList)
-        for x in range(0,len(self.carLane.items)): print(self.carLane.items[x].lpNo, " ", end='')
-        print ("\n-_-_-_-_-_-_-_-_-_")
+        print(self.printList)
 
+        print ("\n----------------")
+
+    def execute(self):
+
+        print("***Car Park Example***")
+        print("*****instructions*****")
+        print("Enter A, D or E")
+        print("A <license number> for Arrive\nD <license number> for departure\nE for exit")
+        print("(ex: A 1)")
+        print("**********************\n")
+        while True:
+
+            try:
+                cmd = input("A <license No> / D <license No> / E\n>")
+                cmd = str.upper(cmd)
+                cmdList = cmd.split(" ")
+
+                if cmdList[0] == "A":
+                    cmdList[1] = int(cmdList[1])
+                    if cmdList[1] not in self.printList:
+                        self.arrive(cmdList[1])
+                    else:
+                        print("There is a car in the car lane that has the same lpNo.")
+
+                elif cmdList[0] == "D":
+                    cmdList[1] = int(cmdList[1])
+                    if cmdList[1] in self.printList:
+                        self.departure(cmdList[1])
+                    else:
+                        print("There is no such car that has lpNo:'", cmdList[1], "'.")
+                elif cmd == "E":
+                    break
+                else:
+                    print("Invalid input.")
+            except:
+                print("Invalid Input. Please Enter correct format.")
+            finally:
+                print("-------------------------------------------\n")
+                self.det()
+                pass
 
 # end of carPark
 
-cp = carPark(10)
-cp.arrive(1)
-cp.arrive(2)
-cp.arrive(3)
-cp.arrive(4)
-cp.arrive(5)
-cp.arrive(6)
-cp.arrive(7)
-cp.arrive(8)
-cp.arrive(9)
-cp.arrive(10)
-cp.arrive(11)
-cp.arrive(12)
-cp.arrive(12)
-cp.arrive(13)
-cp.arrive(14)
-cp.arrive(14)
-cp.arrive(15)
-cp.arrive(17)
-cp.arrive(16)
-cp.arrive(18)
-cp.arrive(19)
-cp.arrive(20)
-cp.arrive(21)
-cp.arrive(22)
-cp.arrive(23)
-cp.arrive(24)
-cp.arrive(25)
-
-cp.departure(5)
-cp.departure(9)
-
-cp.departure(1)
-cp.departure(8)
-cp.departure(10)
-cp.departure(11)
-cp.departure(12)
-cp.departure(13)
-cp.departure(14)
-cp.departure(4)
-cp.departure(15)
-cp.departure(16)
-cp.departure(17)
-cp.departure(18)
-cp.departure(19)
-cp.departure(20)
-cp.departure(21)
-cp.departure(22)
-cp.departure(23)
-cp.departure(24)
-cp.departure(2)
-cp.det()
+c = carPark(10)
+c.execute()
