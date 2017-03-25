58f8f77eb7113347b2d566b3e5169e1d6dcb4bac
ppkavinda <pp.kavinda@gmail.com>
2017-03-25 11:42:24 +0530

    fixed: checking waitList b4 arriving car

--- assignment/carPark.py
+++ assignment/carPark.py
@@ -60,49 +60,55 @@ class carPark:
         else:
             if len(self.waitList) is 0:     # if wait list is empty add car to car lane
                 car = Car(lpNo)
-                print("Car Lane is have room for ", self.maxCars - len(self.carLane.items) - 1, "cars. Added the car 'License No: ", lpNo, "' to the Lane.")
+                print("Car Lane is have room for ", self.maxCars - len(self.carLane.items) , "cars. Added the car 'License No: ", lpNo, "' to the Lane.")
                 self.carLane.enqueue(car)
                 self.printList.append(lpNo)
                 # car.count += 1
 
             else:   # if waiting list has cars add them to the carLane
-                print("Car Lane is have room for ", self.maxCars - len(self.carLane.items) - 1, "cars. Added the car 'License No: ", lpNo, "' to the Lane from waiting list.")
+                print("Car Lane is have room for ", self.maxCars - len(self.carLane.items) , "cars. Added the car 'License No: ", lpNo, "' to the Lane from waiting list.")
                 waitcar = Car(self.waitList.pop(0))
                 self.carLane.enqueue(waitcar)
+                waitcar.count += 1
                 self.printList.append(lpNo)
                 # waitcar.count += 1
 
     # departure method
     def departure(self, target):    # target = target car
         print("<<<<Departing  a car")
-        for _ in range(0, len(self.carLane.items)):
+        for x in range(0, len(self.carLane.items)):
             test = self.carLane.dequeue()   # test = currently checking car
             test.count += 1
-            if test.lpNo is target:             # if currently checking car is the target, skip enq process
+            if test.lpNo == target:             # if currently checking car is the target, skip enq process
+                if x == 0:                      # if first car is the target, skip the loop and just pop first car.
+                    print("Departing the car 'License No: ", test.lpNo, "'.")
+                    print("The car moved '", test.count, "'times through the car lane.")
+                    self.printList.pop(self.printList.index(target))
+                    break
                 print("Departing the car 'License No: ", test.lpNo, "'.")
                 print("The car moved '", test.count, "'times through the car lane.")
-                self.printList.pop(0)
+                self.printList.pop(self.printList.index(target))
                 continue
             else:
                 self.carLane.enqueue(test)  # if not the target, enq back to the carLane
 
-        if len(self.waitList) is not 0:     # if waiting list have any cars, enq them to the carLane
+        if len(self.waitList) != 0:     # if waiting list have any cars, enq them to the carLane
             print("There is room for a car.")
             waitcar = Car(self.waitList.pop(0))
             waitcar.count += 1
             self.carLane.enqueue(waitcar)
             print("Added the car to the 'License No: ", waitcar.lpNo, "'from the waiting list.")
+            self.printList.append(waitcar.lpNo)
 
     # det method for (print)details
     def det(self):
-        print ("------------------")
-        print ("wtlist", self.waitList)
+        print("------------------")
+        print("wtlist", self.waitList)
         print(self.printList)
+        print("\n----------------")
 
-        print ("\n----------------")
-
+    # this is the organizing function
     def execute(self):
-
         print("***Car Park Example***")
         print("*****instructions*****")
         print("Enter A, D or E")
@@ -118,7 +124,7 @@ class carPark:
 
                 if cmdList[0] == "A":
                     cmdList[1] = int(cmdList[1])
-                    if cmdList[1] not in self.printList:
+                    if (cmdList[1] not in self.printList) and (cmdList[1] not in self.waitList):
                         self.arrive(cmdList[1])
                     else:
                         print("There is a car in the car lane that has the same lpNo.")
