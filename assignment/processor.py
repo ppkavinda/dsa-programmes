03867d8fcc4af7ff6e32e5fccc8afd6fcd2af50c
ppkavinda <pp.kavinda@gmail.com>
2017-03-26 10:49:56 +0530

    done allocate

--- assignment/processor.py
+++ assignment/processor.py
@@ -6,33 +6,48 @@ class Node:
         self.processID = processID
         self.hole = True
         self.next = None
+        self.size = size
+
+    def printNode(self):
+        print(self.size)
 
 
 # beginning of class memory
 class Memory:
     def __init__(self, freeMem):
         os = Node("Operating System", 400)
-        self.head = os
-        freeSpace = Node("Free Space", freeMem+1)
-        os.next = None
         self.head.hole = False
+        self.head = os
+
+        freeSpace = Node("Free Space", freeMem+1)
+        freeSpace.hole = True
+        os.next = freeSpace
 
     def checkPExists(self, ps):
         current = self.head
         while current.next is not None:
             if current.processID == ps:
                 return True
-            else:
-                return False
             current = current.next
+        return False
 
     def allocate(self, pID, size):
         if self.checkPExists(pID):
             print("The process: '", pID, "' is already running.")
         else:
-            ps = Node(pID, size)
+            current = self.head
+            while current.next is not None:
+                if current.hole is True:
+                    freeSpace = (current.endAddress + 1) - current.beginAddress
+                    if freeSpace >= size:
+                        newPs = Node(pID, size)
+                        newPs.beginAddress = current.beginAddress
+                        newPs.endAddress = newPs.beginAddress + size - 1
+                        newPs.hole = False
+                        current.beginAddress = newPs.endAddress + 1
+                        break
 
-        pass
+                current = current.next
 
     def terminate(self):
         pass
