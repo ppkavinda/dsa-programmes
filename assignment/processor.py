77036b87c188e4d88f31d921bf73430673b9899a
ppkavinda <pp.kavinda@gmail.com>
2017-03-26 11:40:14 +0530

    done in desktop

--- assignment/processor.py
+++ assignment/processor.py
@@ -8,35 +8,39 @@ class Node:
         self.next = None
         self.size = size
 
-    def printNode(self):
+    def printSize(self):
         print(self.size)
 
+    def printNode(self):
+        print(self.beginAddress, "K - ", self.endAddress, "K :", self.processID)
 
 # beginning of class memory
 class Memory:
     def __init__(self, freeMem):
-        os = Node("Operating System", 400)
+        self.head = Node("Operating System", 400)
         self.head.hole = False
-        self.head = os
-
         freeSpace = Node("Free Space", freeMem+1)
         freeSpace.hole = True
-        os.next = freeSpace
+        freeSpace.beginAddress = self.head.endAddress + 1
+        self.head.next = freeSpace
 
     def checkPExists(self, ps):
         current = self.head
-        while current.next is not None:
+        while current is not None:
             if current.processID == ps:
+                print("T")  # todo
                 return True
             current = current.next
+        print("F")  # todo
         return False
 
     def allocate(self, pID, size):
-        if self.checkPExists(pID):
+        if self.checkPExists(pID) is True:
             print("The process: '", pID, "' is already running.")
+
         else:
             current = self.head
-            while current.next is not None:
+            while current is not None:
                 if current.hole is True:
                     freeSpace = (current.endAddress + 1) - current.beginAddress
                     if freeSpace >= size:
@@ -44,16 +48,37 @@ class Memory:
                         newPs.beginAddress = current.beginAddress
                         newPs.endAddress = newPs.beginAddress + size - 1
                         newPs.hole = False
+                        newPs.processID = pID
                         current.beginAddress = newPs.endAddress + 1
+                        print(1, pID)  # TODO
+
                         break
 
                 current = current.next
+                print(2)  # todo
 
-    def terminate(self):
-        pass
+    def terminate(self, pID):
+        current = self.head
+        while current is not None:
+            if current.processID == pID:
+                if current.next.hole is True:
+                    current.endAddress = current.next.endAddress
+                    current.next = current.next.next
+                current.hole = True
+                current.processID = "Free Space"
+                break
+            current = current.next
+            print("Process ID '", pID, "' is not found.")
 
     def snapshot(self):
-        pass
+        current = self.head
+        while current is not None:
+            current.printNode()
+            # print(current.beginAddress, "K - ", current.endAddress, "K :", current.processID)
+            current = current.next
+
 
 m = Memory(2560)
+m.allocate("P1", 600)
+# m.allocate("P1", 600)
 m.snapshot()
