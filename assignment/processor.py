0038ace83a9679f241f64f6067550d1656788bfe
ppkavinda <pp.kavinda@gmail.com>
2017-03-26 08:58:51 +0530

    editing processof

--- assignment/processor.py
+++ assignment/processor.py
@@ -1,5 +1,52 @@
-a = input("Do you use python\n")
-if a == "Y":
-    print("Greate")
-else:
-    print("Too bad")
+# implementing class Node
+class Node:
+    def __init__(self, processID, size):
+        self.beginAddress = None
+        self.endAddress = None
+        self.processID = processID
+        self.size = size
+        self.hole = True
+        self.next = None
+
+    def printNode(self):
+        print(self.data)
+
+
+# beginning of class memory
+class Memory:
+    def __init__(self, freeMem):
+        self.beginAddress = 0
+        self.endAddress = 2560
+        self.pID = None
+        self.freeMem = freeMem
+        self.head = None
+
+        os = Node("Operating System", 400)
+        self.head = os
+        os.next = None
+
+    def checkPExists(self, ps):
+        current = self.head
+        while current.next is not None:
+            if current.processID == ps:
+                return True
+            else:
+                return False
+            current = current.next
+
+    def allocate(self, pID, size):
+        if self.checkPExists(pID):
+            print("The process: '", pID, "' is already running.")
+        else:
+            ps = Node(pID, size)
+
+        pass
+
+    def terminate(self):
+        pass
+
+    def snapshot(self):
+        pass
+
+m = Memory(2560)
+m.snapshot()
