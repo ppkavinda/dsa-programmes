0f282ee669406742aa96cedd53aa754862694348
ppkavinda <pp.kavinda@gmail.com>
2017-04-08 10:01:45 +0530

    done doubly linked list.

--- linkedList/doubly_linked_list.py
+++ linkedList/doubly_linked_list.py
@@ -16,63 +16,83 @@ class Doubly_Linked_list:
         self.tail = None
         self.prob = self.head
 
-    def insert(self, data_in):
-        new_node = Node(data_in)
-
-        if self.head is None:
-            self.head = new_node
-            self.tail = self.head
-            self.prob = self.head
-
-        elif data_in < self.head.data:
-            new_node.next = self.head
-            self.head.prev = new_node
-            self.head = new_node
-
-        elif data_in > self.tail.data:
-            self.tail.next = new_node
-            new_node.prev = self.tail
-            self.tail = new_node
-
-        else:
-            tmp = self.head
-            while tmp is not self.tail and tmp.data < data_in:
-                tmp = tmp.next
-
-            new_node.next = tmp.next
-            tmp.next.prev = new_node
-            tmp.next = new_node
-            new_node.prev = tmp
+    # def insert(self, data_in):
+    #     new_node = Node(data_in)
+    #
+    #     if self.head is None:
+    #         self.head = new_node
+    #         self.tail = self.head
+    #         self.prob = self.head
+    #
+    #     elif data_in < self.head.data:
+    #         new_node.next = self.head
+    #         self.head.prev = new_node
+    #         self.head = new_node
+    #
+    #     elif data_in > self.tail.data:
+    #         self.tail.next = new_node
+    #         new_node.prev = self.tail
+    #         self.tail = new_node
+    #
+    #     else:
+    #         tmp = self.head
+    #         while tmp is not None and tmp.data < data_in:
+    #             tmp = tmp.next
+    #         place = tmp.prev
+    #
+    #         new_node.next = place.next
+    #         place.next.prev = new_node
+    #         new_node.prev = place
+    #         place.next = new_node
 
     def remove(self, data):
         if self.head is None:
+            print("Error: List is empty.")
             return False
+        elif data == self.head.data:
+            node = self.head
+            self.head = self.head.next
+            del node
+
+        elif data == self.tail.data:
+            node = self.tail
+            self.tail = self.tail.prev
+            del node
+
         elif data < self.prob.data:
             while self.prob is not None and self.prob.data != data:
                 self.prob = self.prob.prev
+                node = self.prob
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            del node
 
         elif data > self.prob.data:
             while self.prob is not None and self.prob.data != data:
                 self.prob = self.prob.next
+                node = self.prob
 
-        self.prob.prev = self.prob.next
-        self.prob.next = self.prob.prev
-
-        # print("Prem:", self.prob.data)
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            del node
 
     def printL(self):
         temp = self.head
-        while temp is not None:
-            print(temp.data)
-            temp = temp.next
-        # print("P:", self.prob.data)
+        if temp is not None:
+            while temp is not self.tail:
+                print(temp.data, " ",  end='')
+                temp = temp.next
+            print(self.tail.data)
+            # print("P:", self.prob.data)
+        else:
+            print("List is Empty.")
 
 
 l = Doubly_Linked_list()
-l.insert(9)
-l.insert(5)
-l.insert(2)
+l.insert(59)
 l.insert(3)
+l.insert(43)
+l.insert(2)
 l.printL()
 print("----")
 
